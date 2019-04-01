4d9d25de2ba6d17cafafd27bc0c63aeb2ce59035
unknown <blazingzeros@gmail.com>
2019-04-01 06:21:52 -0400

    first commit

---
+++ Project/public/index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title>SE_266.55</title>
+    </head>
+    <body>
+        <?php include './navbar.php'; ?>
+            <div class="jumbotron shadow-lg p-3 mb-5 bg-white rounded container-fluid h-100">
+                <div class ="row" style="margin-left: 4%">
+                    <div class="col-">
+                <h1 class="display-4">Project Web Page</h1>
+            <p class="lead" style="margin-top:4%">Welcome to William Connolly's PHP project page!</p>
+            <hr class="my-4">
+            <p>The Nav Bar above contains the Assignments web pages, where this container will have the Git repository and files.</p>
+            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style="margin-bottom: 2%;">
+                PHP Files
+            </button>
+                </div>
+            
+            
+<!-- Git Panel ------------------------------------------------------>
+            <div class="col-4 mb-0">
+            <img class="rounded-circle mx-auto d-block" src="./Content/generic.png" alt="Generic placeholder image" width="140" height="140">
+            <h2 class = "text-center"><a href="https://github.com/echosmith/PHP-SQL-Projects">William Connolly's Git</a></h2>
+            <p class = "text-center">This Link above directly leads to the PHP git repository. Three educational instances used for PHP are below.</p>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="https://www.w3schools.com/php/default.asp">w3schools</a></li>
+                    <li class = "list-group-item"><a href="http://www.php.net/">php.net</a></li>
+                    <li class = "list-group-item"><a href="https://www.learn-php.org/">Learn Php</a></li>
+                </ul>
+        </div>
+            
+                </div>
+            
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    <div class="accordion" id="accordionExample">
+                        <div class="card">
+                          <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#WeekOneFiles" aria-expanded="false" aria-controls="WeekOneFiles">
+                                Week_1
+                              </button>
+                            </h5>
+                          </div>
+                            <div id="WeekOneFiles" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <div class="list-group">
+                                    <a href="../SE266/Week_1/hello.zip" class="list-group-item list-group-item-action">
+                                        Hello
+                                    </a>
+                                    <a href="../SE266/Week_1/FizzBuzz.zip" class="list-group-item list-group-item-action ">FizzBuzz</a>
+                                </div>
+                            </div>
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <?php
+            //include 'apiTest.php';
+        ?>
+        <!-- Optional JavaScript -->
+        <script>
+            var toggler = document.getElementsByClassName("caret");
+            var i;
+
+            for (i = 0; i < toggler.length; i++) {
+                toggler[i].addEventListener("click", function() {
+                    this.parentElement.querySelector(".nested").classList.toggle("active");
+                    this.classList.toggle("caret-down");
+                });
+            }
+        </script>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ Project/public/navbar.php
@@ -0,0 +1,32 @@
+
+    <div class="pos-f-t">
+            <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                    <h5 class="text-white h4">SE_266 William Connolly's</h5>
+                    <span class="text-muted">
+<!-------------------------------------------------- Drop Down Nav Bar -------------------------------------------------------------------------------------------------------------->
+                        <ul class="nav-bar nav">
+                           <a href="index.php" class="btn btn-info btn-lg" style="margin-right: 1%">
+                                <span class="glyphicon glyphicon-home"></span> Home
+                           </a>
+                            <div class="btn-group" style="margin-right: 1%;">
+                                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Week One
+                                </button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="/008002464/public_html/Spring_2019/SE266/Week_1/hello.php">Hello</a>
+                                    <a class="dropdown-item" href="/008002464/public_html/Spring_2019/SE266/Week_1/FizzBuzz.php">FizzBuzz</a>
+                                </div>
+                            </div>
+
+                        </ul>
+                    </span>
+                </div>
+            </div>
+            <nav class="navbar navbar-dark bg-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+            
+        </div>
